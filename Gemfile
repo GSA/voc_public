a3bce50f3fda27a281cdd6364bfb446d884a8990
Ben Miller <bmiller@ctacorp.com>
2011-04-18 14:44:41 +0000

    initial import

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.6'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'jquery-rails'
+gem 'thin'
+
+group :development, :test do 
+	gem 'mysql2'
+	gem 'ruby-debug19'
+	gem 'nifty-generators'
+	gem 'annotate-models'
+end
+gem "mocha", :group => :test
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommentToolPublic::Application.config.secret_token = '6e148eab4b16ecff17a34f4801832ce6ed89cf8746c4048be5c9f74341b38f2d753f3b894eb6fa37410b66f8ff6eb260daaf73ffa15ca0f655ed85dac6c44630'
