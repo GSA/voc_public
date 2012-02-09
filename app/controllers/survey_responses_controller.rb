30e4da3efe50267244b00bc128d6d588b863bc9a
Aaron Stowell <astowell@ctacorp.com>
2012-02-09 05:06:02 +0000

    persist stylesheet to thank you page

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -20,7 +20,7 @@ class SurveyResponsesController < ApplicationController
     @survey_response.save!
     
     #TODO: redirect to a thank you page
-    redirect_to thank_you_surveys_path, :stylesheet => params[:stylesheet]
+    redirect_to :controller => 'surveys', :action => 'thank_you', :stylesheet => params[:stylesheet]
   end
 
 end
