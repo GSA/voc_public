dc0e3ece84ba458f2ca925c7d6992fd6a1faf3c8
Aaron Stowell <astowell@ctacorp.com>
2012-02-09 07:24:26 +0000

    adding google analytics

---
+++ public/javascripts/gatc-survey.js
@@ -0,0 +1,16 @@
+var _gaq = _gaq || [];
+_gaq.push(function() {_gat._anonymizeIp();});
+_gaq.push(
+['_setAccount', 'UA-28750072-1'],
+['_setDomainName', '.healthcare.gov'],
+['_setAllowLinker', true],
+['_setAllowHash', false],
+['_setMaxCustomVariables', 10],
+['_trackPageview'],
+['_trackPageLoadTime']
+);
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
