6337c6cd7a191d9116e7de9654948fc9f8dc847a
Juan Alvarado <jalvarado@ctacorp.com>
2012-03-13 02:54:53 +0000

    surveys#show will default to showing the published version.  There will only be one published version for a survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -14,7 +14,7 @@ class SurveysController < ApplicationController
   # GET /surveys/1.xml
   def show
     @survey = Survey.find(params[:id])
-    @survey_version = params[:version].blank? ? @survey.newest_version : get_survey_version(@survey, params[:version])
+    @survey_version = params[:version].blank? ? @survey.published_version : get_survey_version(@survey, params[:version])
     
     respond_to do |format|
       format.html # show.html.erb
