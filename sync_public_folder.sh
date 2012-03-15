d6959df19b6f1230a04d375e6b6396eac2913b97
Ben Miller <bmiller@ctacorp.com>
2012-03-15 01:34:39 +0000

    fixed permission issue

--- sync_public_folder.sh
+++ sync_public_folder.sh
@@ -1,1 +1,1 @@
-rsync -rcv --chmod=ug=rwX -e "ssh -i /appdata/id_rsa" ./public/ hhsweb@comment-ws-01:/var/www/voc/public/
+rsync -rcv --chmod=uog=rX -e "ssh -i /appdata/id_rsa" ./public/ hhsweb@comment-ws-01:/var/www/voc/public/
