b16e5da6524539e83eb03d105e0b31a7b8c122ef
Juan Alvarado <jalvarado@ctacorp.com>
2012-08-02 17:26:21 +0000

    custom thank you page should now use the correct layout when rendering

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -15,8 +15,8 @@ class SurveyResponsesController < ApplicationController
     SurveyResponse.delay.process_response params[:response], params[:survey_version_id]
 
     @survey_version = SurveyVersion.find(params[:survey_version_id])
-    if @survey_version.present? && !@survey_version.thank_you_page.blank?
-      render :text => @survey_version.thank_you_page, :stylesheet => params[:stylesheet]
+    if @survey_version.present? && @survey_version.thank_you_page.present?
+      render :text => @survey_version.thank_you_page, :stylesheet => params[:stylesheet], :layout => 'application'
     else
       redirect_to :controller => 'surveys', :action => 'thank_you', :stylesheet => params[:stylesheet]
     end
