cb62b603be85ec41c3b839651da0413ff819c599
Juan Alvarado <jalvarado@ctacorp.com>
2012-08-27 12:56:48 +0000

    update the rsync script per bishal's instructions

--- sync_public_folder.sh
+++ sync_public_folder.sh
@@ -1,1 +1,2 @@
 rsync -rcv --chmod=uog=rX -e "ssh -i /appdata/id_rsa" ./public/ hhsweb@comment-ws-01:/var/www/voc/public/
+rsync -rcv --chmod=uog=rX -e "ssh -i /appdata/id_rsa-comment-ws-02" ./public/ hhsweb@comment-ws-02:/var/www/voc/public/
