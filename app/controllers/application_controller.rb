f926fe448120c2717cd6b10a9c5bd4fb138f4b7e
Juan Alvarado <jalvarado@ctacorp.com>
2012-10-19 20:40:18 +0000

    add allowed headers for CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,11 @@ class ApplicationController < ActionController::Base
   # For all responses in this controller, return the CORS access control headers.
 
   def cors_set_access_control_headers
+    Rails.logger.info "Allowing request from #{request.env['HTTP_ORIGIN']}"
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
+    headers['Access-Control-Allow-Headers'] = 'Pragma, Cache-Control'
   end
 
   # If this is a preflight OPTIONS request, then short-circuit the
@@ -18,9 +20,10 @@ class ApplicationController < ActionController::Base
 
   def cors_preflight_check
     if request.method == :options
+      Rails.logger.info "Received OPTIONS request from #{request.env['HTTP_ORIGIN']}"
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Pragma'
       headers['Access-Control-Max-Age'] = '1728000'
       render :text => '', :content_type => 'text/plain'
     end
