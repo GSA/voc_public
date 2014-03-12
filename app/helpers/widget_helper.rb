041ad6ab6c7fbb8beaae7ee21d701e7016e1fa96
Angel N. Sciortino <asciortino@ctacorp.com>
2014-03-12 18:08:26 -0500

    Work on invitation

--- app/helpers/widget_helper.rb
+++ app/helpers/widget_helper.rb
@@ -2,4 +2,24 @@
 #
 # View helpers for Javascript widget functionality.
 module WidgetHelper
+  def invitation_text
+    text = @survey.invitation_text
+    text ||= "Would you like to take a survey?<br/>"
+  end
+
+  def invitation_accept_button_text
+    if @survey.invitation_accept_button_text.empty? 
+      "Yes"
+    else
+      @survey.invitation_accept_button_text
+    end
+  end
+
+  def invitation_reject_button_text
+    if @survey.invitation_reject_button_text.empty?
+      "No"
+    else
+      @survey.invitation_accept_button_text
+    end
+  end
 end
