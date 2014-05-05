6357ed4e7c08af068c7673853288fbf7f420a19a
Juan Alvarado <jalvarado@ctacorp.com>
2014-05-05 14:05:17 -0400

    use the locale from the survey if present

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@
 # View helper repository for helpers used across functional areas of the application.
 module ApplicationHelper
   def language_attribute
-    "en"
+    if @survey.present? && @survey.locale.present?
+      @survey.locale
+    else
+      "en"
+    end
   end
 end
