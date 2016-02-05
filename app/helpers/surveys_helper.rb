69acb0bd79d4b20bb81402351a7ba282ff079108
Juan Alvarado <jalvarado@ctacorp.com>
2016-02-04 21:45:42 -0500

    Pass  to set_next_page

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -32,7 +32,7 @@ module SurveysHelper
   def generate_onclick(element, page, answer)
     onclick = ""
     if element.assetable.question_content.flow_control
-      onclick += "VOC.set_next_page(#{page.page_number}, #{answer.page.try(:page_number) || (element.page.page_number + 1)});"
+      onclick += "VOC.set_next_page(this, #{page.page_number}, #{answer.page.try(:page_number) || (element.page.page_number + 1)});"
     end
 
     if element.assetable.auto_next_page
