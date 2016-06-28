e803518d64eafec080014c19a80ae594c00516f9
Jake Vose <jake.vose@gmail.com>
2016-06-28 10:15:52 -0400

    containerizing. requires admin app repo to control from docker-compose

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,17 @@
-# Be sure to restart your server when you modify this file.
+secret_token_loaded = false
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CommentToolPublic::Application.config.secret_token = '6e148eab4b16ecff17a34f4801832ce6ed89cf8746c4048be5c9f74341b38f2d753f3b894eb6fa37410b66f8ff6eb260daaf73ffa15ca0f655ed85dac6c44630'
+begin
+  require 'yaml_erb_loader'
+
+  SecretConfig = YamlErbLoader.load_from_config("#{Rails.root}/config/secret_settings.yml")
+
+  unless SecretConfig.blank? or (token = SecretConfig["secret"]).nil?
+    CommentToolPublic::Application.config.secret_token = token
+    secret_token_loaded = true
+  end
+rescue
+end
+
+unless secret_token_loaded
+  raise "Missing configuration file for secret key settings (config/secret_settings.yml) #{$!}"
+end
